35eec92daf70ee30ff5ba77dfaa180d57db80f33
shahriar100 <shahriar.hasan100@gmail.com>
2021-01-15 21:05:42 +0600

    basic view and url roution, jsonresponse, args, kwargs

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('tweet/<int:tweet_id>', views.tweet_detail_view, name='detail-view'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+
+    return HttpResponse("<h1> hello </h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    Rest Api view
+    Consume by Javascript or Swift
+    return json data
+    """
+
+    print(args, kwargs)
+
+    data = {
+        'id': tweet_id,
+    }
+
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=data['id'])
+        # append content to the dictionary
+        data['content'] = obj.content
+        # data['image_path'] = obj.image.url
+    except:
+        data['message'] = 'Not Found'
+        status = 404
+
+    # json.dumps content_tyoe="application.json"
+    return JsonResponse(data, status=status)
